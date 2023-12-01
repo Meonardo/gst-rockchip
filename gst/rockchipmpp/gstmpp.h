4c2abddf6aa709107f2a9557454ab666cc522019
Meonardo <sisxme@gmail.com>
2023-12-01 18:11:39 +0800

    Update for build librga.

--- gst/rockchipmpp/gstmpp.h
+++ gst/rockchipmpp/gstmpp.h
@@ -26,8 +26,13 @@
 #include <gst/allocators/gstdmabuf.h>
 
 #ifdef HAVE_RGA
-#include <rga/rga.h>
-#include <rga/RgaApi.h>
+
+#ifdef ANDROID
+#undef ANDROID
+#endif
+
+#include <rga.h>
+#include <RgaApi.h>
 #endif
 
 #include <rockchip/rk_mpi.h>
